0190bf8093808bdd60d518ba097001c572e295ca
Zhongyi Chen <zhongyi753@gmail.com>
2019-09-27 15:27:51 -0500

    initialize server files

--- Server/server.ts
+++ Server/server.ts
@@ -1,1 +1,26 @@
-console.log('Hello world');
+import * as path from 'path';
+import * as http from 'http';
+import * as WebSocket from 'ws';
+import * as express from 'express';
+import * as serveStatic from 'serve-static';
+
+const server = http.createServer();
+const wss = new WebSocket.Server({ port: 8080 });
+
+console.log('Starting server');
+
+var app = express();
+app.use(express.static('public'));
+app.use(serveStatic(path.resolve(__dirname, 'public')));
+server.on('request', app);
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+    });
+
+    ws.send('something');
+});
+
+
+app.listen(8000);
