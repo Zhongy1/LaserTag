585b83a577f58ffe24f53594bb52b41a4d2908a4
Rashad <torashad99@gmail.com>
2019-10-23 00:20:36 -0500

    Added IR_ESP8266 file and created function prototype for emitHex function

---
+++ Arduino/IR_ESP8266/IR_ESP8266.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int emitHex(long int input){
+    return 1;
+  }
+
+}
