2c07929836bef4a6afe82e51221a2b699c6432ec
Akash Pillai <akashp807@gmail.com>
2019-10-30 17:34:33 -0500

    Implement StringDecoder method to convert hexadecimal messages

--- Server/string-decoder.ts
+++ Server/string-decoder.ts
@@ -1,6 +1,14 @@
 export class StringDecoder {
     constructor() {}
+
     static toJSON(str: string) {
         return {};
+    }
+
+    static hexToString(hex: string) {
+        let str: string = '';
+        for (let i = 0; (i < hex.length && hex.substr(i, 2) !== '00'); i += 2)
+            str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+        return str;
     }
 }
