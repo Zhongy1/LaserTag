acf555daead9dc91de9befc895d6d5148e7edf27
Akash Pillai <akashp807@gmail.com>
2019-10-31 11:51:40 -0500

    Add helper method for hex string validation

--- Server/string-decoder.ts
+++ Server/string-decoder.ts
@@ -5,10 +5,21 @@ export class StringDecoder {
         return {};
     }
 
-    static hexToString(hex: string) {
+    private static isHex(hex: string): boolean {
+        let regex: RegExp = new RegExp('^[0-9a-fA-F]+$');
+        return regex.test(hex);
+    }
+
+    static hexToString(hex: string): string {
         let str: string = '';
-        for (let i = 0; (i < hex.length && hex.substr(i, 2) !== '00'); i += 2)
-            str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-        return str;
+
+        if (this.isHex(hex)) {
+            for (let i = 0; (i < hex.length && hex.substr(i, 2) !== '00'); i += 2)
+                str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+            return str;
+        }
+        else {
+            return null;
+        }
     }
 }
