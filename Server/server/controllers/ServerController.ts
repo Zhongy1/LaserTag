683db3a45f81a4538fe8653e8fc148dd1bc28669
fdemp <dempseyfrederick@gmail.com>
2020-02-25 16:43:44 -0600

    init branch

---
+++ Server/server/controllers/ServerController.ts
@@ -0,0 +1,9 @@
+import WebSocket from 'ws';
+import Player from './Player';
+import WaitingRoom from './WaitingRoom';
+
+export default class ServerController {
+    public handle(ws: WebSocket) {
+
+    }
+}
