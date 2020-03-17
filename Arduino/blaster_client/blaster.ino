6edb17713d6231ea437163a3e6a182c1e4657320
Anthony <aventu6@uic.edu>
2020-03-17 16:14:28 -0500

    Changed the Structure of Files For Arduino

---
+++ Arduino/blaster_client/blaster.ino
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <Blaster.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  //handleColorDetails()
+
+  //handleTriggerAndOutput()
+  
+  //handleScreenUpdate()
+
+  //handleBatteryCheck()
+
+  //handleServerCommunication()
+}
